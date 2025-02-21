0f285513ef4f869392fbb1b077d607c801f4157e
Ostor <dolgorukov.dm@gmail.com>
2025-02-21 16:42:33 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,6 @@
-(function() {
-    window.location.href = "https://bibi-car.ru";
-})();
+var firstName = prompt('Введите ваше имя:');
+var lastName = prompt('Введите вашу фамилию:');
+var inn = prompt('Введите ваш ИНН:');
+
+console.log("Привет,", firstName, lastName + "!");
+console.log("Ваш ИНН:", inn);
